259ed6d27fc404bc998d8423a185e988fed3397e
David Pinto <daxtery11@gmail.com>
2021-07-20 17:37:38 +0100

    Add options to remove deprecation warnings

--- src/feedback/feedback.module.ts
+++ src/feedback/feedback.module.ts
@@ -5,6 +5,13 @@ import { FeedbackService } from './feedback.service';
 import { FeedbackResolver } from './feedback.resolver';
 import { QueryFeedback, QueryFeedBackSchema } from './schemas/query-feedback.schema';
 
+import mongoose from 'mongoose';
+
+// NOTE: We define these options because of deprecation warnings
+
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
 @Module({
   imports: [FlaskModule,
     MongooseModule.forFeature([
